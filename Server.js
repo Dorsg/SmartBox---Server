9fdb1493360096702fbe81c3356ff8f17e67e0e1
Dorsg <70431706+Dorsg@users.noreply.github.com>
2022-04-13 19:16:59 +0300

    update server

--- Server.js
+++ Server.js
@@ -2,14 +2,13 @@ const express = require('express')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
-const app = express()
+const app = express();
 const uri = "mongodb+srv://smartBox:smartBox@cluster0.sgf80.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-app.use(cors())
+
+app.use(cors());
 app.use(express.json());
 
-// OK
 app.get('/', async (req, res)=> {
     try{
         await client.connect();
@@ -28,110 +27,58 @@ app.get('/', async (req, res)=> {
     }
 })
 
-// TODO not working
+app.get('/signin', async (req, res) => {
+    try{
+        await client.connect();
+        const database = client.db("smart_box");
+        const collection = database.collection("users");  // get reference to the collection
+        const user = await collection.findOne({ email: req.body.email });
+
+        if (user) {
+            const password = await collection.findOne({ password: req.body.password });
+            if (password){
+                res.status(200).send("User credentials are valid");
+            } else{
+                res.status(401).send("User password is not valid");
+            }
+        } else{
+            res.status(404).send("User mail is not exists");
+        }
+
+    } catch (e){
+        console.log(e);
+    } finally {
+        await client.close()
+    }
+});
+
 app.post('/signup', async (req, res) => {
-    const cred = {
-                   email: req.body.email,
-                   password : req.body.password
-                 };
+    const credentials = { email: req.body.email,
+                          password: req.body.password  };
 
     try{
         await client.connect();
         const database = client.db("smart_box");
-        const collection = database.collection("users");
-        let query = { email: cred.email };
+        const collection = database.collection("users");  // get reference to the collection
+        const user = await collection.findOne({ email: req.body.email });
 
-        const user = await collection.find(query).toArray(function(err, result) {
-
-            console.log(result);
-        });
-
+        if (user) {
+            res.status(401).send("User is already exist in the system");
+        } else{
+            const addedUser = await collection.insertOne(credentials);
+            if(addedUser) {
+                res.status(200).send("User and password has been successfully added to the system");
+            } else{
+                res.status(500).send("User credentials were OK but an error has occurred on the server");
+            }
+        }
     } catch (e){
         console.log(e);
     } finally {
         await client.close()
     }
-
-
-})
+});
 
 app.listen(3000, ()=> {
     console.log('app is running on port 3000');
-})
-
-/*app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                return db.select('*').from('users')
-                    .where('email', '=', req.body.email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})*/
-
-
-/*app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({id})
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not found')
-            }
-        })
-        .catch(err => res.status(400).json('error getting user'))
-})
-
-app.put('/image', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
-            // entries[0] --> this used to return the entries
-            // TO
-            // entries[0].entries --> this now returns the entries
-            res.json(entries[0].entries);
-        })
-        .catch(err => res.status(400).json('unable to get entries'))
-})*/
-
-
-
-
-
-
-
-
-
-/*
-app.get('/', function (req, res) {
-
-    getCollection();
-
-})
-
-async function getCollection(){
-    try{
-        await client.connect();
-        let databaseList = await client.db().admin().listDatabases();
-        console.log(databaseList);
-    } catch (e){
-        console.log(e);
-    } finally {
-        await client.close()
-    }
-}
-
-app.listen(3000)*/
+});
